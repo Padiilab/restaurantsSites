4e92c2e467952277cdfac77dba68fddb999697bc
Padiilab <55683707+Padiilab@users.noreply.github.com>
2020-12-13 00:11:56 +0200

    Added second page

--- my-react-app/src/Component/About/About.js
+++ my-react-app/src/Component/About/About.js
@@ -1,13 +1,25 @@
 import React,{Component} from "react";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import spinner from "../Home/Images/spinner1.gif";
 export default class about extends Component {
     render() {
         return (
             <>
-                <div>
+                <div className={"content"}>
                     <Header/>
-                    it'sTest
+                    <div className={'wrapperHome'}>
+
+                        <div className={"container containerHome"}>
+                            <div className={"contentHome"}>
+                                Аля вторая страница
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
+                <Footer/>
             </>
         );
     }
