75013e4d60c1e84403900c4066c3941393fc1b14
Padiilab <55683707+Padiilab@users.noreply.github.com>
2021-01-24 10:54:22 +0200

    Change some styles of restaurants carts. Improvements page layout.

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {BrowserRouter as Router, BrowserRouter, Link, NavLink} from "react-router-dom";
 import {Navbar} from 'react-bootstrap'
 import './Header.css'
+import logo from './RestView.png';
 
 export default class header extends Component {
     render() {
@@ -9,8 +10,9 @@ export default class header extends Component {
             <div className={'headerContainer'}>
                 <div className={"navbarWrapper"}>
                 <Navbar className={'headerRoute'}>
+                    <Link to={'/'}> <img className={'logoHeader'} src={logo} /></Link>
                     <Link className={'HeaderLink'} to={'/'}>Home</Link>
-                    <Link className={'HeaderLink'} to={'/about'}>Restaurants</Link>
+                    <Link className={'HeaderLink'} to={'/'}>Restaurants</Link>
                 </Navbar>
                 </div>
             </div>
