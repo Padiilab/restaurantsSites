dfd1dec4b1e574c76e77abd089074401557363d0
Padiilab <55683707+Padiilab@users.noreply.github.com>
2021-10-27 00:29:21 +0300

    changed edit-page

--- src/pages/edit-page/edit-page.js
+++ src/pages/edit-page/edit-page.js
@@ -37,6 +37,13 @@ export const EditPage = ({ editData, onBackToHome, updateListRestaurant }) => {
               });
               updateListRestaurant();
               onBackToHome();
+            })
+            .catch(() => {
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+              });
             });
         }
       });
